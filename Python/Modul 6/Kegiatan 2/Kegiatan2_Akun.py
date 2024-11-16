fd350f48bc0dec821ada2a001d52ea943c41d6ba
Winki <163889519+Winki372@users.noreply.github.com>
2024-11-16 15:01:18 +0700

    Create Kegiatan2_Akun.py

---
+++ Python/Modul 6/Kegiatan 2/Kegiatan2_Akun.py
@@ -0,0 +1,16 @@
+##Program Akun 
+##Dibuat oleh Thoriq Saiful Muhsinin
+import random
+
+Nama = 'Thoriq Saiful Muhsinin'
+Tanggal_Lahir = '25 September 2005'
+Angka_Acak = ''.join([str(random.randint(0,5)) for i in range(3)])
+Nama_Singkat = f"{Nama[:6]} {Nama[7]}.{Nama[14]}"
+Username = (Nama[0])+(Tanggal_Lahir[:2])+(Tanggal_Lahir[-4:])
+Password = (Nama[:3])+(Angka_Acak)
+#------------------------------------------------------------------
+print(f"Nama : {Nama}")
+print(f"Tanggal Lahir : {Tanggal_Lahir}")
+print(f"Nama Singkat : {Nama_Singkat}")
+print(f"Username : {Username}")
+print(f"Password Sementara : {Password}")
