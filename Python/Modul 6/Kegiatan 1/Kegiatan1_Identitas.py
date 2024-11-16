01564792852fc813f057d8fba0de622a0c817c66
Winki <163889519+Winki372@users.noreply.github.com>
2024-11-16 14:59:44 +0700

    Create Kegiatan1_Identitas.py

---
+++ Python/Modul 6/Kegiatan 1/Kegiatan1_Identitas.py
@@ -0,0 +1,23 @@
+##PROGRAM IDENTITAS DIRI. DIBUAT OLEH
+#THORIQ SAIFUL MUHSININ
+Name = 'THORIQ SAIFUL MUHSININ'
+Adrress = 'Cantel Wetan, Sragen Tengah, Sragen'
+Hobby = 'Watching Film'
+Age = int(19)
+Favorite_Colour = 'Red, Blue, Grey'
+Shoes_Size = int(42)
+Religion ='Islam'
+Food = 'Kwetiaw'
+Gender = 'Male'
+Country = 'Indonesia'
+
+print(f"Nama Ku : {Name}")
+print(f"Alamat rumah saya : {Adrress}")
+print(f"Kegiatan yang saya sukai saya : {Hobby}")
+print(f"Saya telah berada di bumi selama : {Age} tahun")
+print(f"Warna yang saya sukai : {Favorite_Colour}")
+print(f"Ukuran sepatu saya : {Shoes_Size}")
+print(f"Agamaku : {Religion}")
+print(f"Makanan yang saya sukai : {Food}🤤")
+print(f"Aku : {Gender}")
+print(f"Aku asli orang : {Country}")
