4ba556516de5ce60df3147f60f1d2d4a8546a0e6
Winki <163889519+Winki372@users.noreply.github.com>
2024-12-01 21:47:49 +0700

    Add files via upload

---
+++ Python/Modul 8/Kegiatan 1/Kegiatan1_MembuatModul_1.py
@@ -0,0 +1,28 @@
+profile = { 'nama':'Thoriq Saiful Muhsinin',
+            'nim':'*******029',
+            'alamat':'Surakarta',
+            'gender':'Laki-Laki', 
+            'umur': '17',
+            'kesibukan': 'Student',
+            'hobi': 'menonton film/series',}
+
+def nama():
+    print(f'Nama: {profile["nama"]}')
+
+def nim():
+    print(f'NIM: {profile["nim"]}')
+
+def alamat():
+    print(f'Alamat: {profile["alamat"]}')
+
+def gender():
+    print(f'Gender: {profile["gender"]}')
+
+def umur():
+    print(f'Umur: {profile["umur"]}')
+
+def kesibukan():
+    print(f'Kesibukan: {profile["kesibukan"]}')
+
+def hobi():
+    print(f'Hobi: {profile["hobi"]}')
