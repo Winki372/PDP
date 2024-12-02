14cdac31d6e86c74ac80a1f3ef3f832031e7004a
Winki <163889519+Winki372@users.noreply.github.com>
2024-12-02 11:34:49 +0700

    Add files via upload

---
+++ Python/Modul 9/Kegiatan 3/kegiatan3.py
@@ -0,0 +1,14 @@
+import shelve
+
+f = open("L300240029", 'r')
+data = f.readlines()
+
+nim = data[0].strip()
+tanggal_lahir = data[1].strip()
+nama = data[2].strip()
+
+f = shelve.open("Thoriq")
+f['nim']= nim.strip() 
+f['tgl_lahir']=tanggal_lahir.strip()
+f['nama']=nama.strip()
+f.close()
