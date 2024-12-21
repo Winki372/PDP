c3ffb083e71862f5913215c4785450e7acdeced9
Winki <163889519+Winki372@users.noreply.github.com>
2024-12-21 11:28:18 +0700

    Create index.py

---
+++ Python/Modul 12/Kegiatan 1 dan 2/cgi-bin/index.py
@@ -0,0 +1,37 @@
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA DIRI</title>
+</head>
+<body>
+    <h2>DATA DIRI</h2>
+    <table>
+    <img src="https://akcdn.detik.net.id/community/media/visual/2023/12/22/film-studio-ghibli-karya-hayao-miyazaki-8_34.jpeg?w=700&q=90" style="float:left; width:200px; height:300px; margin-right:10px;">
+    <tr>
+        <td>Nama : </td>
+        <td>THORIQ SAIFUL MUHSININ</td>
+    </tr>
+    <tr>
+        <td>Alamat tinggal: </td>
+        <td>Surakarta</td>
+    </tr>
+    <tr>
+        <td>Tempat, tanggal lahir: </td>
+        <td>Sragen, dd/mm/yyyy</td>
+    </tr>
+    <tr>
+        <td>Tempat wisata favorit: </td>
+        <td>Australia</td>
+    </tr>
+    <tr>
+        <td>Motto: </td>
+        <td>Liveeeeee</td>
+    </tr>
+    </table>
+</body>
+</html>
+"""
+print(html)
